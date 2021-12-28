c49078432aa7f4e0ebb1405cae0a4e81bc8c92d6
Karishad <44437832+Karishad@users.noreply.github.com>
2021-12-28 13:12:33 -0600

    Created BandNameGenerator in Day1

---
+++ Day1-BandNameGenerator/day1.py
@@ -0,0 +1,12 @@
+# This program will take in two inputs and concatenate the strings
+# The main concepts that we are practicing 
+# - Print statements 
+# - Variables 
+# - User input 
+# - Printing Variables
+
+
+print("We will be creating you a band name")
+firstword = input("Give me the first work in your band name\n")
+secondword = input("Give me the second word in your band name\n")
+print(firstword + " "+ secondword)
