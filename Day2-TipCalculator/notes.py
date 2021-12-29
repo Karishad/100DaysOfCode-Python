8b19a5423711cf4970c92c4c72620d7ff46f5492
Karishad <44437832+Karishad@users.noreply.github.com>
2021-12-28 22:30:00 -0600

    Added TipCalculator + Notes into Day2

---
+++ Day2-TipCalculator/notes.py
@@ -0,0 +1,21 @@
+#Data types
+
+#Strings
+# "Hello"[0] = H
+print("Hello"[0])
+
+# This is subscripting
+
+# Integers
+print("123" + "456")
+# note versus 
+print(123+456)
+
+# Type Checking
+
+num_char = len("Finding Jesus -_-")
+num_char1 = "Finding Jesus -o-"
+print(type(num_char))
+print(type(num_char1))
+
+# Type Conversions
---
+++ Day2-TipCalculator/tipcalculator.py
@@ -0,0 +1,7 @@
+# This program will take in the total cost of a bill, how many people to split and tip percentage of the bill 
+# The program will output the cost per person  
+# The main concepts that we are practicing 
+# - Data Types
+# - Type Conversions
+# - f-Strings
+
